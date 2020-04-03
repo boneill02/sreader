ab270a12b3785c32d9902eaf3e85c57fbdbdcb6e
Ben O'Neill <ben@benoneill.xyz>
2020-04-02 20:46:13 -0400

    More features :DDD

--- gofeed.go
+++ gofeed.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/marcusolsson/tui-go"
+	"github.com/marcusolsson/tui-go/wordwrap"
 	"github.com/SlyMarbo/rss"
 	"jaytaylor.com/html2text"
 )
@@ -25,53 +26,84 @@ func main() {
 
 	get_feed(feed)
 
-	feedbox := tui.NewTable(0, 0) // create table for feeds
+	feedbox := tui.NewTable(0, 0) // create table for feed names
 	feedbox.SetFocused(true)
 
 	for _, item := range items {
 		feedbox.AppendRow(
 			tui.NewLabel(item.Title),
-			tui.NewLabel(item.Date.String()),
-			tui.NewLabel(item.Link),
 		)
 	}
-
-	title := tui.NewLabel("")
-	date := tui.NewLabel("")
-	url := tui.NewLabel("")
 
 	content := tui.NewLabel("")
 	content.SetSizePolicy(tui.Preferred, tui.Expanding)
 
+	contentarea := tui.NewScrollArea(content)
+	contentview := tui.NewVBox(contentarea)
+
+	info := tui.NewLabel("")
+
 	feedbox.OnSelectionChanged(func(t *tui.Table) {
 		e := items[t.Selected()]
-		title.SetText(e.Title)
-		date.SetText(e.Date.String())
-		url.SetText(e.Link)
+		info.SetText(e.Title)
 
 		feedtext, err := html2text.FromString(e.Summary, html2text.Options{PrettyTables: true})
 		if err != nil {
 			panic(err)
 		}
-		content.SetText(feedtext)
+		content.SetText(wordwrap.WrapString(feedtext, 80))
 	})
 
 	feedbox.Select(0)
 
-	root := tui.NewVBox(
-		feedbox,
-		tui.NewPadder(1, 0, tui.NewLabel("")),
-		tui.NewSpacer(),
-		content,
-	)
+	feedpad := tui.NewLabel("")
+	feedpad.SetSizePolicy(tui.Preferred, tui.Expanding)
+
+	feedview := tui.NewVBox(feedbox, feedpad)
+
+	root := tui.NewVBox(feedview)
 
 	ui, err := tui.New(root)
+
+
+	view := 0
 
 	if err != nil {
 		panic(err)
 	}
 
 	ui.SetKeybinding("q", func() { ui.Quit() })
+	ui.SetKeybinding("h", func() {
+		if view == 0 {
+			ui.Quit()
+		} else if view == 1 {
+			ui.SetWidget(feedview)
+			contentarea.ScrollToTop() // Autoscroll back
+			view = 0
+		}
+	})
+	ui.SetKeybinding("j", func() { contentarea.Scroll(0, 1) })
+	ui.SetKeybinding("k", func() { contentarea.Scroll(0, -1) })
+	ui.SetKeybinding("l", func() {
+		ui.SetWidget(contentview)
+		view = 1
+	})
+
+	ui.SetKeybinding("Esc", func() {
+		if view == 0 {
+			ui.Quit()
+		} else if view == 1 {
+			ui.SetWidget(feedview)
+			contentarea.ScrollToTop() // Autoscroll back
+			view = 0
+		}
+	})
+	ui.SetKeybinding("Enter", func() {
+		ui.SetWidget(contentview)
+		view = 1
+	})
+	ui.SetKeybinding("Up", func() { contentarea.Scroll(0, -1) })
+	ui.SetKeybinding("Down", func() { contentarea.Scroll(0, 1) })
 
 	if err := ui.Run(); err != nil {
 		panic(err)
